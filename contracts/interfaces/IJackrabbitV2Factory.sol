82556babeffdb3a285e25bc51dd307df1e0f1124
Lamont V. <lamontsprin@aol.com>
2024-09-11 07:08:26 -0400

    Update ICoffheeV2Factory.sol

--- contracts/interfaces/IJackrabbitV2Factory.sol
+++ contracts/interfaces/IJackrabbitV2Factory.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.5.0;
 
-interface IJackrabbitV2Factory {
+interface ICoffheeV2Factory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
     function feeTo() external view returns (address);
